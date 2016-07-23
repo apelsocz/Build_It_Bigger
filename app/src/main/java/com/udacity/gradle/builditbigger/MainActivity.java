c1cdef932db7766ac2c873551e5a9b8cbe6c6216
apelsoczi <apelsoczi@archlinux.us>
2016-07-22 23:52:30 -0400

    feat: Java Library base implementation for jokes
    - Added a Java Module with a class to supply jokes.
    - Module added to project’s settings.gradle file.
    - Declared project dependency between app and JokeSmith.
    - Populate Toast message with a string from JokeSmith.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.jokesmith.JokeSmith;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -40,8 +42,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeSmith joke = new JokeSmith();
+        Toast.makeText(this, joke.getJoke(), Toast.LENGTH_SHORT).show();
     }
-
-
 }
---
+++ jokes/src/main/java/com/jokesmith/JokeSmith.java
@@ -0,0 +1,9 @@
+package com.jokesmith;
+
+import java.lang.String;
+
+public class JokeSmith {
+    public String getJoke() {
+        return "JokeSmith Java Library joke!";
+    }
+}
