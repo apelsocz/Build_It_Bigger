9e9f8498069a908cc918703343c553eea2efcbba
apelsoczi <apelsoczi@archlinux.us>
2016-07-24 17:01:57 -0400

    refactor: Added Jokes
    - defined jokes for Java Library module

--- jokes/src/main/java/com/jokesmith/JokeSmith.java
+++ jokes/src/main/java/com/jokesmith/JokeSmith.java
@@ -1,9 +1,21 @@
 package com.jokesmith;
 
-import java.lang.String;
+import java.util.Random;
 
 public class JokeSmith {
     public String getJoke() {
-        return "JokeSmith Java Library joke!";
+        String[] jokes = new String[] {
+                "Programmer (noun):\n\nA machine that turns coffee into code",
+                "Algorithm (noun):\n\nA word used by programmers when they do not want to explain what they did",
+                "Q: What do you call a programmer from Finland?\n\nA: Nerdic",
+                "Q: Where is a programmer's favorite place to hangout?\nA: Foo Bar",
+                "Q: Why did the programmer quit his job?\n\nA: Because he didn't get arrays",
+                "Q: 0 is false and 1 is true, right?\n\nA: 1",
+                "Q: Why do Java programmers have to wear glasses?\n\nA: Because they do not C#",
+                "99 bugs in the code,\n99 bugs in the code,\nfix 1 bug ... compile again,\n101 bugs in the code",
+                "Hide and seek champion...\n;\nsince 1958",
+                "['hip','hip']"
+        };
+        return jokes[new Random().nextInt(jokes.length)];
     }
 }
